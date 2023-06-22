e4e608fffe01fe12cec2728866c51192ad3b50e3
Rutuj99 <rutujbg99@gmail.com>
2023-06-22 21:32:52 +0530

    take33

--- assets/js/script.js
+++ assets/js/script.js
@@ -245,8 +245,8 @@ srtop.reveal('.education .box', { interval: 200 });
 srtop.reveal('.work .box', { interval: 200 });
 
 /* SCROLL EXPERIENCE */
-srtop.reveal('.experience .timeline', { delay: 400 });
-srtop.reveal('.experience .timeline .container', { interval: 400 });
+// srtop.reveal('.experience .timeline', { delay: 100 });
+// srtop.reveal('.experience .timeline .container', { interval: 100 });
 
 /* SCROLL CONTACT */
 srtop.reveal('.contact .container', { delay: 400 });
